5708dcc952dbb4681e2444711689fd23e153083c
Wade <wade@pop-os.localdomain>
2020-03-13 19:56:37 +0300

    done app

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ function customHttp() {
     get(url, cb) {
       try {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.addEventListener('load', () => {
+        xhr.open("GET", url);
+        xhr.addEventListener("load", () => {
           if (Math.floor(xhr.status / 100) !== 2) {
             cb(`Error. Status code: ${xhr.status}`, xhr);
             return;
@@ -14,7 +14,7 @@ function customHttp() {
           cb(null, response);
         });
 
-        xhr.addEventListener('error', () => {
+        xhr.addEventListener("error", () => {
           cb(`Error. Status code: ${xhr.status}`, xhr);
         });
 
@@ -26,8 +26,8 @@ function customHttp() {
     post(url, body, headers, cb) {
       try {
         const xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
-        xhr.addEventListener('load', () => {
+        xhr.open("POST", url);
+        xhr.addEventListener("load", () => {
           if (Math.floor(xhr.status / 100) !== 2) {
             cb(`Error. Status code: ${xhr.status}`, xhr);
             return;
@@ -36,7 +36,7 @@ function customHttp() {
           cb(null, response);
         });
 
-        xhr.addEventListener('error', () => {
+        xhr.addEventListener("error", () => {
           cb(`Error. Status code: ${xhr.status}`, xhr);
         });
 
@@ -50,13 +50,143 @@ function customHttp() {
       } catch (error) {
         cb(error);
       }
-    },
+    }
   };
 }
 // Init http module
 const http = customHttp();
 
-//  init selects
-document.addEventListener('DOMContentLoaded', function() {
-  M.AutoInit();
+const newsService = (function() {
+  const apiKey = "551955855c2c4350b0d96d2c8f1e5fe3";
+  const apiUrl = "http://newsapi.org/v2";
+
+  return {
+    topHeadLines(country = "ua", cb) {
+      http.get(
+        `${apiUrl}/top-headlines?country=${country}&apiKey=${apiKey}`,
+        cb
+      );
+    },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+    }
+  };
+})();
+
+//Elements
+const form = document.forms["newsControls"];
+const countrySelect = form.elements["country"];
+const searchInput = form.elements["search"];
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  loadNews();
 });
+
+//  init selects
+document.addEventListener("DOMContentLoaded", function() {
+  M.AutoInit();
+  loadNews();
+});
+
+// Load news function
+function loadNews() {
+  showLoader();
+
+  const country = countrySelect.value;
+  const searchText = searchInput.value;
+
+  if (!searchText) {
+    newsService.topHeadLines(country, onGetResponse);
+  } else {
+    newsService.everything(searchText, onGetResponse);
+  }
+}
+
+//Function on get response from server
+function onGetResponse(err, res) {
+  removePreloader();
+
+  if (err) {
+    showAlert(err, "error-msg");
+    return;
+  }
+
+  if (!res.articles.length) {
+    // show empty message
+    return;
+  }
+
+  console.log(res);
+  renderNews(res.articles);
+}
+
+//Function render news
+function renderNews(news) {
+  const newsContainer = document.querySelector(".news-container .row");
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  }
+  let fragment = "";
+
+  news.forEach(newsItem => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+
+  newsContainer.insertAdjacentHTML("afterbegin", fragment);
+}
+
+// Clear using container function
+function clearContainer(container) {
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+}
+
+//News item template function
+function newsTemplate({ urlToImage, title, url, description }) {
+  return `
+  <div class="col s12">
+    <div class="card">
+      <div class="card-image">
+        <img src="${urlToImage}">
+        <span class="card-title">${title || ""}</span>
+      </div>
+      <div class="card-content">
+        <p>${description || ""}</p>
+      </div>
+      <div class="card-action">
+        <a href="${url}">Read more</a>
+      </div>
+    </div>
+  </div>
+  `;
+}
+
+//Show some alert function
+function showAlert(msg, type = "success") {
+  M.toast({ html: msg, classes: type });
+}
+
+// Show loader function
+function showLoader() {
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <div class="progress">
+      <div class="indeterminate"></div>
+    </div>
+    `
+  );
+}
+
+// Remove loader function
+function removePreloader() {
+  const loader = document.querySelector(".progress");
+  if (loader) {
+    loader.remove();
+  }
+}
